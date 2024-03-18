863d9443ed482d9be1d377cec305f64cebd6bdfa
Adam Bedient <adambedient@gmail.com>
2024-03-18 17:05:16 -0400

    Establish basic cards

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { useState } from 'react';
+import Ideas from '../Ideas/Ideas'
+
+function App () {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+  ]
+
+  const [ideas, setIdeas] = useState(dummyIdeas)
+  return (
+    <main>
+      <h1>IdeaBox</h1>
+      <Ideas 
+        ideas={ideas}
+      />
+    </main>
+  )
+}
+
+export default App;
